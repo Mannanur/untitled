6fb1f22d9437c2d2c48763068628808a3f308a2b
nur <nurlan.mannapbaev@gmail.com>
2022-02-26 19:40:02 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner nur = new Scanner(System.in);
+        int a = nur.nextInt();
+        System.out.println(a+10);
+
+
+	    }
+}
